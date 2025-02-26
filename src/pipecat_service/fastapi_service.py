fe2a660a567e614c2eb20ce7f7d2973df06b9cd0
good-riverdeer <riverdeer.youn@gmail.com>
2025-02-26 18:58:03 +0900

    add fastapi_service draft

--- src/pipecat_service/fastapi_service.py
+++ src/pipecat_service/fastapi_service.py
@@ -1,85 +1,55 @@
-import requests
+from typing import List
 
-from pipecat.frames.frames import Frame, LLMMessagesFrame, LLMFullResponseStartFrame, LLMFullResponseEndFrame
+from pipecat.frames.frames import Frame, LLMMessagesFrame, LLMFullResponseStartFrame, LLMFullResponseEndFrame, LLMTextFrame
 from pipecat.processors.frame_processor import FrameDirection
-from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext, OpenAILLMContextFrame
+from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
 
-from pipecat.services.ai_services import LLMService
+from pipecat.services.openai import BaseOpenAILLMService
 
 from openai.types.chat import ChatCompletionMessageParam
 
-
-class FastAPILLMContext:
-    def __init__(
-        self,
-        messages: list[ChatCompletionMessageParam] | None = None,
-    ):
-        self._messages: list[ChatCompletionMessageParam] = messages if messages else []
-
-    @staticmethod
-    def from_messages(messages: list[dict]) -> "FastAPILLMContext":
-        context = FastAPILLMContext()
-        
-        for message in messages:
-            if "name" not in message:
-                message["name"] = message["role"]
-            context.add_message(message)
-        return context
-
-    @property
-    def messages(self) -> list[ChatCompletionMessageParam]:
-        return self._messages
-
-    def add_message(self, message: ChatCompletionMessageParam):
-        self._messages.append(message)
-
-    def add_messages(self, messages: list[ChatCompletionMessageParam]):
-        self._messages.extend(messages)
-
-    def set_messages(self, messages: list[ChatCompletionMessageParam]):
-        self._messages[:] = messages
-
-    def get_messages(self) -> list[ChatCompletionMessageParam]:
-        return self._messages
+from client.client import AgentClient
 
 
-class BaseFastAPIService(LLMService):
+class FastAPIService(BaseOpenAILLMService):
 
     def __init__(
         self,
-        req_url: str,
+        *,
+        model: str,
+        base_url: str,
         **kwargs,
     ):
-        super().__init__(**kwargs)
-        self._req_url = req_url
+        super().__init__(model=model, base_url=base_url, **kwargs)
+        self.set_model_name(model)
+        self._client = self.create_client(base_url=base_url)
 
-    async def _process_context(self, context: OpenAILLMContext):
+    def create_client(self, base_url=None, **kwargs):
+        return AgentClient(base_url=base_url, agent="chatbot")
+
+    async def get_chat_completions(self, context, messages: List[ChatCompletionMessageParam]):
+        chunk = self._client.astream(
+            message=context.messages[-1]["content"],
+            model=self.model_name,
+            # TODO: thread-id를 구분할 수 있는 ID 정의 필요
+            thread_id="random number",
+            agent_config={},
+            stream_tokens=True,
+        )
+        return chunk
+
+    async def _process_context(self, context: str):
+
         await self.start_ttfb_metrics()
 
-        messages = context.messages
+        chunk_stream = await self._stream_chat_completions(context)
 
-        response = requests.post(
-            url=self._req_url,
-            json={
-                "state": {
-                    "messages": [
-                        {
-                            "type": "human",
-                            "content": content,
-                        }
-                    ],
-                    "is_last_step": False,
-                    "remaining_steps": 10,
-                },
-                "config": {
-                    "configurable": {
-                        "thread_id": message.author.name,
-                        "user_id": message.author.id,
-                    }
-                },
-            },
-            stream=True
-        )
+        async for chunk in chunk_stream:
+            await self.stop_ttfb_metrics()
+            if isinstance(chunk, str):
+                # TODO: 답변이 끝나면 agent-service-toolkit의 ChatMessage 객체가
+                # chunk로 흘러들어옴.
+                await self.push_frame(LLMTextFrame(chunk))
 
     async def process_frame(self, frame: Frame, direction: FrameDirection):
         await super().process_frame(frame, direction)
